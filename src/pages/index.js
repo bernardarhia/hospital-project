cc3510e511ea9709e4bb0450f7b8f7e151c20ecc
Bernard Arhia <bernardarhia@gmail.com>
2020-10-10 04:28:12 +0000

    fixed navigation not loading

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,7 @@
 import React from "react"
 import Showcase from "../components/Showcase";
 // import Showca
-import "../styles/scss/app.scss";
+import "../styles/scss/app.css";
 export default function Home() {
   return (
     <>
