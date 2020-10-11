2d0abdfd33acb711cbf3e2d8fd48385b8b22fdd3
Bernard Arhia <bernardarhia@gmail.com>
2020-10-11 15:53:41 +0000

    initial commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,10 @@
 import React from "react"
-import Showcase from "../components/Showcase";
-// import Showca
-import "../styles/scss/app.css";
+
+
 export default function Home() {
   return (
     <>
-      <Showcase />
+      
     </>
   )
 }
