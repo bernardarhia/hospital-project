cf7d6e9f90e176e4925f6fd7402406c703855999
Bernard Arhia <bernardarhia@gmail.com>
2020-10-15 04:43:06 +0000

    added styling to 404 page and fixed is responsiveness

--- src/pages/404.jsx
+++ src/pages/404.jsx
@@ -1,22 +1,38 @@
-import { Link } from 'gatsby'
-import React from 'react'
-
+import { Link } from "gatsby"
+import React from "react"
+import Layout from "../Layout"
+import Container from "../Container"
+import Navbar from "../components/universal/Navbar"
+import Footer from "../components/universal/Footer"
 const ErrorPage = () => {
-    return (
+  return (
+    <Layout>
+      <Navbar />
+      <Container>
+        {/*  404 content*/}
         <div className="error__page">
-            <div className="error__page-content">
+          <div className="error__page-content">
             <div className="error__page-img">
-                <img src="https://res.cloudinary.com/everich1/image/upload/v1602735744/hopital%20project/Img_de28su.png" alt=""/>
+              <img
+                src="https://res.cloudinary.com/everich1/image/upload/v1602735744/hopital%20project/Img_de28su.png"
+                alt=""
+              />
             </div>
             <div className="error__page-text">
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et.</p>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor incididunt ut labore et.
+              </p>
             </div>
             <div className="error__page-redirect">
-                <Link to="/">Go home</Link>
+              <Link to="/">Go home</Link>
             </div>
-            </div>
+          </div>
         </div>
-    )
+      </Container>
+      <Footer />
+    </Layout>
+  )
 }
 
 export default ErrorPage
